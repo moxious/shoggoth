f207ace081664049134db99b5b2f4f7995bafb31
moxious <david.allen@grafana.com>
2023-04-24 10:25:30 -0400

    added search method, and get topic by discourse link

--- discourse.py
+++ discourse.py
@@ -1,5 +1,6 @@
 import os
 from pydiscourse import DiscourseClient
+from datetime import date, timedelta
 
 def topics_with_single_post(topics):
     return [
@@ -23,6 +24,18 @@ class Topic:
             "title": self.data['title'],
             "content": top_post['cooked'],
         }
+
+    def __dict__(self):
+        return self.data
+
+    def tags(self): 
+        return self.data['tags']
+
+    def is_tagged(self): 
+        return len(self.tags()) > 0
+
+    def has_replies(self):
+        return self.data['posts_count'] > 1
 
     def get_posts(self):
         return self.data['post_stream']['posts']
@@ -49,11 +62,41 @@ class CommunitySupport():
         [slug, id] = path[3:].split('/')
         return { "slug": slug, "id": id }
 
-    def latest_topics_no_replies(self):
-        response = self.client.latest_topics()        
-        topics = topics_with_single_post(response['topic_list']['topics'])
-        return [
-            Topic(self.client.topic(topic_id=topic['id'], slug=topic['slug']))
-            for topic in topics
-            if topic['posts_count'] == 1
-        ]
+    def get_topic_for_link(self, link):
+        bits = self.parse_link(link)
+        return Topic(self.client.topic(slug=bits['slug'], topic_id=bits['id']))
+
+    """Search for posts that match a particular string"""
+    def search(self, q='', page=1, recent_only=True, no_replies=True):
+        search_string = q
+        if no_replies:
+            search_string = search_string + " min_posts:1 max_posts:1"
+        if recent_only:
+            search_string = search_string + " after:%s" % str(date.today() - timedelta(days=4))
+
+        # https://community.grafana.com/search?q=after%3A2023-04-20%20min_posts%3A1%20max_posts%3A1&page=1
+        response = self.client.search(term='', q=search_string, page=page)
+        return self.post_generator(response['posts'])        
+
+    def post_generator(self, list):
+        """Async calls to client.post take time so we implement this as a generator"""
+        idx = 0
+        while idx < len(list):
+            current = list[idx]
+            ks = sorted(current.keys())
+            
+            print(ks)
+            yield Topic(self.client.post(post_id=current['id'], topic_id=current['topic_id']))
+            idx += 1
+
+    def topic_generator(self, list):
+        """Async calls to client.post take time so we implement this as a generator"""
+        idx = 0
+        while idx < len(list):
+            current = list[idx]
+            yield Topic(self.client.topic(topic_id=current['id'], slug=current['slug']))
+            idx += 1
+
+    def latest_topics(self):
+        response = self.client.latest_topics()
+        return self.topic_generator(response['topic_list']['topics'])
